d43e0ae6f8282523869e9c7b35bdb41f872eb29b
Allen <allen.chen@silksoftware.com>
2018-08-17 18:19:05 +0800

    player

---
+++ src/pages/Player.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="player">
+    <div class="background">
+      <img data-v-35618e92="" width="100%" height="100%"
+           src="https://y.gtimg.cn/music/photo_new/T002R300x300M000000gCu9F4cwL7S.jpg?max_age=2592000">
+    </div>
+    <audio src=""></audio>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "player"
+  }
+</script>
+
+<style scoped>
+  .fixed-top{
+    width: 100%;
+    position: fixed;
+    top: 0;
+    display: flex;
+    z-index: 10;
+  }
+  .player {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 150;
+    height: 100vh;
+    width: 100vw;
+    background: #222222;
+    filter: blur(3px);
+  }
+.background{
+
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: .6;
+  -webkit-filter: blur(20px);
+  filter: blur(20px);
+}
+</style>
--- src/pages/SingerDetail.vue
+++ src/pages/SingerDetail.vue
@@ -1,16 +1,41 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail">this is child</div>
+    <div class="singer-detail">
+      <div class="fixed-top">
+        <div>
+         <i class="icon-back" @click="goback" v-if="!show"></i>
+          <i class="icon-back rotate" @click="hide" v-if="show"></i>
+        </div>
+        <div class="col-fff singer_name">
+          {{singer_detail.singer_name}}
+        </div>
+      </div>
+      <div class="rel">
+        <div class="filter"></div>
+        <img :src="singer_detail.singer_img" alt="">
+      </div>
+      <div class="song-list" ref="song_ul">
+        <ul >
+          <li v-for="(item,index) in singer_detail.list" class="song-li" @click="toPlay">
+            <h2>{{item.musicData.songname}}</h2>
+            <p>专辑名：{{item.musicData.albumname}}</p>
+          </li>
+        </ul>
+      </div>
+      <Player v-if="show"></Player>
+    </div>
   </transition>
 </template>
 
 <script>
+  import Player from '@/pages/Player'
   import jsonp from 'jsonp'
   export default {
     name: "singer-detail",
     data(){
         return {
-          singer_detail:{}
+          singer_detail:{},
+          show:false
         }
     },
     created() {
@@ -22,15 +47,118 @@
           prefix: 'jp',
           name: 'jp1'
         }, (err, data) => {
+          data.data.singer_img = `https://y.gtimg.cn/music/photo_new/T001R300x300M000${url}.jpg?max_age=2592000`
           this.singer_detail = data.data
             console.log(data.data)
 
         })
       },
+    mounted(){
+      this.$refs.song_ul.addEventListener("scroll",function(){
+       var scrolltop = this.scrollTop
+        var chcke = document.getElementsByClassName("c_height").length
+        if(scrolltop>0 && chcke === 0){
+         this.className += ' c_height'
+        }else if(scrolltop ==0){
+          this.className = ' song-list'
+        }
+      })
+    },
+    methods:{
+      goback:function(){
+        this.$router.go(-1)
+      },
+      toPlay:function(){
+        this.show = true
+      },
+      hide(){
+        this.show = !this.show
+      }
+
+    },
+    components:{
+      Player:Player
+    }
   }
 </script>
 
 <style scoped>
+  .rotate{
+    transform: rotate(-90deg);
+  }
+  .filter{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(7,17,27,.4);
+  }
+  .singer_name{
+    position: absolute;
+    top: 0;
+    left: 10%;
+    z-index: 40;
+    width: 80%;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+    text-align: center;
+    line-height: 40px;
+    font-size: 18px;
+    color: #fff;
+  }
+  .fixed-top{
+    width: 100%;
+    position: fixed;
+    top: 0;
+    display: flex;
+    z-index: 160;
+  }
+  .icon-back:before {
+    content: "\E911";
+  }
+  .icon-back{
+    display: block;
+    padding: 10px;
+    font-size: 22px;
+    color: #ffcd32;
+  }
+  .c_height{
+    height: 87vh!important;
+    /*transform: translate(0,-50vh);*/
+  }
+  .song-list>ul{
+    padding-bottom: 60px;
+  }
+  .song-li h2{
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+    color: #fff;
+    font-size: 14px;
+  }
+  .song-li p{
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+    margin-top: 4px;
+    color: hsla(0,0%,100%,.3);
+    font-size: 14px;
+  }
+  .song-li{
+    height: 64px;
+  }
+  .song-list{
+    padding: 20px 30px;
+    height: 50vh;
+    overflow-y: scroll;
+    -webkit-overflow-scrolling: touch;
+    background: #222;
+    transition: all linear 0.2s;
+    position: fixed;
+    bottom: 0;
+  }
   .slide-enter-active, .slide-leave-active {
     transition: all 0.3s
   }
@@ -46,6 +174,6 @@
     left: 0;
     right: 0;
     bottom: 0;
-    background: #ffffff;
+    background: #222;
   }
 </style>
